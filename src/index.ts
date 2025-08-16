08a161c227c6fca9fe4b31c488bc0bc01b308867
ilya Pinchuk <deffokorn71@gmail.com>
2025-08-16 14:53:07 +0300

    added swagger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import express = require('express');
+import express from 'express';
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+
 
 const jsonBody = express.json()
-
 const app = express()
 const port = process.env.PORT || 8080;
 
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Courses API',
+            version: '1.0.0',
+            description: 'API для работы с курсами'
+        },
+        servers: [
+            {url: `http://localhost:${port}`}
+        ]
+    },
+    apis: process.env.NODE_ENV === 'production'
+        ? ['./dist/**/*.js']    // Heroku prod: смотреть JS в dist
+        : ['./**/*.ts']         // Dev: ts-node
+};
 
-app.use(jsonBody)
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use(jsonBody).use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 const db = {
     courses: [
         {
@@ -28,6 +48,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+/**
+ * @openapi
+ * /courses:
+ *   get:
+ *     tags:
+ *      - Courses
+ *     summary: Get list
+ *     responses:
+ *       200:
+ *         description: success
+ */
 app.get('/courses', (req, res) => {
     let foundCourses = db.courses;
     if (req.query.title) {
@@ -36,6 +67,25 @@ app.get('/courses', (req, res) => {
     res.json(foundCourses);
 })
 
+/**
+ * @openapi
+ * /courses/{id}:
+ *   get:
+ *     tags:
+ *       - Courses
+ *     summary: Get by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Course not found
+ */
 app.get('/courses/:id', (req, res) => {
     const foundCourse = db.courses.find(x => x.id === +req.params.id);
     if (!foundCourse) {
@@ -44,6 +94,28 @@ app.get('/courses/:id', (req, res) => {
     res.json(foundCourse);
 })
 
+/**
+ * @openapi
+ * /courses:
+ *   post:
+ *     tags:
+ *     - Courses
+ *     summary: Create
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Success
+ *       400:
+ *         description: Missing title
+ */
 app.post('/courses', (req, res) => {
     if (!req.body.title) {
         return res.status(400).json({message: 'Missing title'});
@@ -56,6 +128,25 @@ app.post('/courses', (req, res) => {
     res.status(201).json(course);
 })
 
+/**
+ * @openapi
+ * /courses/{id}:
+ *   delete:
+ *     tags:
+ *     - Courses
+ *     summary: Delete by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Course not found
+ */
 app.delete('/courses/:id', (req, res) => {
     const foundCourse = db.courses.find(x => x.id === +req.params.id);
     if (!foundCourse) {
@@ -65,6 +156,34 @@ app.delete('/courses/:id', (req, res) => {
     res.status(200).json({message: 'Success'});
 })
 
+/**
+ * @openapi
+ * /courses/{id}:
+ *   put:
+ *    tags:
+ *     - Courses
+ *     summary: Update by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Course not found
+ */
 app.put('/courses/:id', (req, res) => {
     const foundCourse = db.courses.find(x => x.id === +req.params.id);
     if (!foundCourse) {
@@ -73,6 +192,7 @@ app.put('/courses/:id', (req, res) => {
     foundCourse.title = req.body.title;
     res.status(200).json(foundCourse);
 })
+
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
